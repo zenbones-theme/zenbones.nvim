aba8ffb53e190dad91f797a91b0be526aa13e34b
Michael Chris Lopez <hello@michaelchris.space>
2021-08-26 13:30:00 +0800

    add lualine theme
    - pretty basic but this should do for now

---
+++ lua/lualine/themes/zenbones.lua
@@ -0,0 +1,31 @@
+local t = require "zenbones"
+
+return {
+	normal = {
+		a = { bg = t.PmenuSbar.bg.hex, fg = t.Folded.fg.hex, gui = "bold" },
+		b = { bg = t.PmenuSel.bg.hex, fg = t.Identifier.fg.hex },
+		c = { bg = t.StatusLine.bg.hex, fg = t.StatusLine.fg.hex },
+	},
+
+	insert = {
+		a = { bg = t.DiffAdd.bg.hex, fg = t.Folded.fg.hex, gui = "bold" },
+	},
+
+	command = {
+		a = { bg = t.ColorColumn.bg.hex, fg = t.Folded.fg.hex, gui = "bold" },
+	},
+
+	visual = {
+		a = { bg = t.Search.bg.hex, fg = t.Folded.fg.hex, gui = "bold" },
+	},
+
+	replace = {
+		a = { bg = t.DiffDelete.bg.hex, fg = t.Folded.fg.hex, gui = "bold" },
+	},
+
+	inactive = {
+		a = { bg = t.StatusLineNC.bg.hex, fg = t.StatusLineNC.fg.hex, gui = "bold" },
+		b = { bg = t.StatusLineNC.bg.hex, fg = t.StatusLineNC.fg.hex },
+		c = { bg = t.StatusLineNC.bg.hex, fg = t.StatusLineNC.fg.hex },
+	},
+}
