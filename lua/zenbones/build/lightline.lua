f6012fbdf557bd82160a99cc0f3020a304150845
Michael Chris Lopez <hello@michaelchris.space>
2021-09-06 18:02:17 +0800

    add warning and error for lightline

--- lua/zenbones/build/lightline.lua
+++ lua/zenbones/build/lightline.lua
@@ -1,27 +1,10 @@
 local template = [[let s:p = {'normal': {}, 'inactive': {}, 'insert': {}, 'replace': {}, 'visual': {}, 'tabline': {}}
 
-let s:nord0 = "#2E3440"
-let s:nord1 = "#3B4252"
-let s:nord2 = "#434C5E"
-let s:nord3 = "#4C566A"
-let s:nord4 = "#D8DEE9"
-let s:nord5 = "#E5E9F0"
-let s:nord6 = "#ECEFF4"
-let s:nord7 = "#8FBCBB"
-let s:nord8 = "#88C0D0"
-let s:nord9 = "#81A1C1"
-let s:nord10 = "#5E81AC"
-let s:nord11 = "#BF616A"
-let s:nord12 = "#D08770"
-let s:nord13 = "#EBCB8B"
-let s:nord14 = "#A3BE8C"
-let s:nord15 = "#B48EAD"
-
 let s:p.normal.left = [ [ "${common_fg}", "${normal_a_bg}", "bold" ], [ "${common_fg}", "${normal_b_bg}" ] ]
 let s:p.normal.middle = [ [ "${normal_c_fg}", "${normal_c_bg}" ] ]
 let s:p.normal.right = [ [ "${common_fg}", "${normal_b_bg}" ], [ "${common_fg}", "${normal_b_bg}" ] ]
-let s:p.normal.warning = [ [ s:nord1, s:nord13 ] ]
-let s:p.normal.error = [ [ s:nord1, s:nord11 ] ]
+let s:p.normal.warning = [ [ "${warning_fg}", "${warning_bg}" ] ]
+let s:p.normal.error = [ [ "${error_fg}", "${error_bg}" ] ]
 
 let s:p.inactive.left =  [ [ "${inactive_fg}", "${inactive_bg}" ], [ "${inactive_fg}", "${inactive_bg}" ] ]
 let s:p.inactive.middle = [ [ "${inactive_fg}", "${inactive_bg}" ] ]
@@ -61,5 +44,9 @@ return {
 		tabline_right_fg = theme.Normal.fg.hex,
 		tabsel_bg = theme.Normal.bg.hex,
 		tabsel_fg = theme.Normal.fg.hex,
+		warning_bg = theme.LspDiagnosticsVirtualTextWarning.bg.hex,
+		warning_fg = theme.LspDiagnosticsVirtualTextWarning.fg.hex,
+		error_bg = theme.LspDiagnosticsVirtualTextError.bg.hex,
+		error_fg = theme.LspDiagnosticsVirtualTextError.fg.hex,
 	},
 }
