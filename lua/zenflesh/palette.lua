9ddeeff8df85af599faa81040eca5389b135d807
Michael Chris Lopez <hello@michaelchris.space>
2021-09-13 17:01:50 +0800

    adjust saturation again

--- lua/zenflesh/palette.lua
+++ lua/zenflesh/palette.lua
@@ -4,10 +4,10 @@ local hsluv = lush.hsluv
 return {
 	sand = hsluv(39, 10, 10),
 	stone = hsluv(230, 7, 75),
-	leaf = hsluv(103, 65, 54),
-	water = hsluv(236, 80, 53),
-	rose = hsluv(4, 40, 53),
-	wood = hsluv(26, 58, 54),
-	blossom = hsluv(318, 34, 56),
-	sky = hsluv(204, 76, 58),
+	leaf = hsluv(103, 63, 54),
+	water = hsluv(236, 78, 53),
+	rose = hsluv(4, 38, 53),
+	wood = hsluv(26, 56, 54),
+	blossom = hsluv(318, 32, 56),
+	sky = hsluv(204, 71, 58),
 }
