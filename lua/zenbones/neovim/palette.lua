6956eb9fd710da2fd691b9fb943745b210a272a3
Michael Chris Lopez <hello@michaelchris.space>
2021-10-09 17:24:14 +0800

    adjust neovim light leaf

--- lua/zenbones/neovim/palette.lua
+++ lua/zenbones/neovim/palette.lua
@@ -3,12 +3,14 @@ local hsluv = lush.hsluv
 
 local M = {}
 
+local zenbones_p = require "zenbones.palette"
 local zenbones = vim.tbl_extend("keep", {
 	-- bg = hsluv "#d3e4db" -- --accent-bg-color
 	bg = hsluv "#e7eee8", -- --bg-color
 	fg = hsluv "#202e18", -- --accent-color
 	leaf = hsluv "#8fff6d", -- need to make green more prominent
-}, require "zenbones.palette")
+	leaf = zenbones_p.leaf.sa(24).li(8), -- need to make green more prominent
+}, zenbones_p)
 M.zenbones = zenbones
 
 local zenflesh = vim.tbl_extend("keep", {
