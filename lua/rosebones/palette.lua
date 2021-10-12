ecdf4dc736b16eb1746503994b1f6734fb15786f
Michael Chris Lopez <hello@michaelchris.space>
2021-10-12 10:47:38 +0800

    adjust rose-pine default fg

--- lua/rosebones/palette.lua
+++ lua/rosebones/palette.lua
@@ -6,7 +6,7 @@ local M = {}
 
 M.light = util.palette_extend({
 	bg = hsluv(57.9, 49.8, 96.5), -- base
-	fg = hsluv(14.5, 32, 36), -- rose, base: 14.5, 51.1, 63.3
+	fg = hsluv(14.5, 32, 34), -- rose, base: 14.5, 51.1, 63.3
 	rose = hsluv(355.6, 38.5, 51.9), -- love
 	leaf = hsluv(228.3, 84.5, 41.5), -- pine
 	wood = hsluv(45.4, 90.6, 70.8), -- gold
@@ -19,7 +19,7 @@ M.light = util.palette_extend({
 M.dark = util.palette_extend {
 	bg = hsluv(271.2, 20.2, 8.5), -- base
 	bg_warm = hsluv(268.9, 24.5, 13.9), -- base moon
-	fg = hsluv(14.8, 21, 82), -- rose, base: 2, 55, 83
+	fg = hsluv(14.8, 23, 86), -- rose, base: 14.8, 59.9, 80.3
 	rose = hsluv(357.2, 75.7, 62.6), -- love
 	leaf = hsluv(227.7, 81.8, 45.8), -- pine
 	wood = hsluv(53.2, 78.8, 81.3), -- gold
