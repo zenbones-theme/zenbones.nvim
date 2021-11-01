71aa08560954f5b84ee4589a35ad232577d46cbc
Michael Chris Lopez <hello@michaelchris.space>
2021-11-01 09:05:40 +0800

    breaking: remove all zenflesh artifacts

--- lua/zenflesh/init.lua
+++
@@ -1,5 +0,0 @@
-local generator = require "zenbones.specs"
-local p = require "zenflesh.palette"
-
-vim.notify("zenflesh is deprecated. Please use zenbones and set background=dark.", vim.log.levels.WARN, { title = "zenbones" })
-return generator.generate(p, "dark", generator.get_global_config "zenflesh")
