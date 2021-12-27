66b8d9a9a3482094ade1362e204d86b23e3da3fe
Thuan Pham <phamtanthuan2311@gmail.com>
2021-12-27 09:16:32 +0700

    fix(windows_terminal): theme name

--- lua/zenbones/shipwright/runners/windows_terminal.lua
+++ lua/zenbones/shipwright/runners/windows_terminal.lua
@@ -1,25 +1,25 @@
 local template = [[{
-"name": "zenbonse",
-"foreground": "$fg",
-"background": "$bg",
-"selectionBackground": "$selection_bg",
-"cursorColor": "$cursor_bg",
-"black": "$black",
-"red": "$red",
-"green": "$green",
-"yellow": "$yellow",
-"blue": "$blue",
-"purple": "$magenta",
-"cyan": "$cyan",
-"white": "$white",
-"brightBlack": "$bright_black",
-"brightRed": "$bright_red",
-"brightGreen": "$bright_green",
-"brightYellow": "$bright_yellow",
-"brightBlue": "$bright_blue",
-"brightPurple": "$bright_magenta",
-"brightCyan": "$bright_cyan",
-"brightWhite": "$bright_white"
+    "name": "$name",
+    "foreground": "$fg",
+    "background": "$bg",
+    "selectionBackground": "$selection_bg",
+    "cursorColor": "$cursor_bg",
+    "black": "$black",
+    "red": "$red",
+    "green": "$green",
+    "yellow": "$yellow",
+    "blue": "$blue",
+    "purple": "$magenta",
+    "cyan": "$cyan",
+    "white": "$white",
+    "brightBlack": "$bright_black",
+    "brightRed": "$bright_red",
+    "brightGreen": "$bright_green",
+    "brightYellow": "$bright_yellow",
+    "brightBlue": "$bright_blue",
+    "brightPurple": "$bright_magenta",
+    "brightCyan": "$bright_cyan",
+    "brightWhite": "$bright_white"
 }
 ]]
 
@@ -32,7 +32,7 @@ run(
 	transform.colorscheme_to_term_colors,
 	function(colors)
 		return vim.tbl_extend("keep", colors, { name = name })
-	end
+	end,
 	function(colors)
 		local text = helpers.apply_template(template, colors)
 		return { text }
