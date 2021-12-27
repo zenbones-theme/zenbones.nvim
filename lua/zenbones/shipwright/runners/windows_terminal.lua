0bf9c5a489ccb7a41f80447f2c82bec8bb095da4
Thuan Pham <40050527+thuanpham2311@users.noreply.github.com>
2021-12-28 06:03:53 +0700

    feat(windows terminal): supported (#68)
    * feat(windows terminal): hello world
    
    * fix(windows terminal): magenta
    
    * fix(windows_terminal): wrong bright black var, missing dir (close #67)
    
    * fix(windows_terminal): cursor color
    
    * ref(windowns terminal): order by content, no comment
    
    * Update lua/zenbones/shipwright/runners/windows_terminal.lua
    
    Co-authored-by: Michael Chris Lopez <hello@michaelchris.space>
    
    * fix(windows_terminal): theme name
    
    Co-authored-by: Michael Chris Lopez <hello@michaelchris.space>

---
+++ lua/zenbones/shipwright/runners/windows_terminal.lua
@@ -0,0 +1,43 @@
+local template = [[{
+    "name": "$name",
+    "foreground": "$fg",
+    "background": "$bg",
+    "selectionBackground": "$selection_bg",
+    "cursorColor": "$cursor_bg",
+    "black": "$black",
+    "red": "$red",
+    "green": "$green",
+    "yellow": "$yellow",
+    "blue": "$blue",
+    "purple": "$magenta",
+    "cyan": "$cyan",
+    "white": "$white",
+    "brightBlack": "$bright_black",
+    "brightRed": "$bright_red",
+    "brightGreen": "$bright_green",
+    "brightYellow": "$bright_yellow",
+    "brightBlue": "$bright_blue",
+    "brightPurple": "$bright_magenta",
+    "brightCyan": "$bright_cyan",
+    "brightWhite": "$bright_white"
+}
+]]
+
+local helpers = require "shipwright.transform.helpers"
+
+---@diagnostic disable: undefined-global
+-- selene: allow(undefined_variable)
+run(
+	{ specs, p, term },
+	transform.colorscheme_to_term_colors,
+	function(colors)
+		return vim.tbl_extend("keep", colors, { name = name })
+	end,
+	function(colors)
+		local text = helpers.apply_template(template, colors)
+		return { text }
+	end,
+	{ overwrite, string.format("extras/windows_terminal/%s.json", name) }
+)
+-- selene: deny(undefined_variable)
+---@diagnostic enable: undefined-global
