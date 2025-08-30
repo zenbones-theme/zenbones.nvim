befe707a7f2ade659f7fae22e80be54c4247c4b8
blackhat-hemsworth <dougcarmody93@gmail.com>
2025-08-29 19:20:02 -0500

    adding random options specific to set backgrounds
    skeleton
    
    checking about name
    
    checking approach
    
    setting to dark
    
    light parity

---
+++ colors/randombones_light.lua
@@ -0,0 +1,26 @@
+if vim.g.colors_name then
+	vim.api.nvim_command [[highlight clear]]
+end
+
+vim.g.colors_name = "randombones_dark"
+
+local util = require "zenbones.util"
+local colorschemes = util.get_colorscheme_list()
+
+math.randomseed(os.time())
+
+local colorscheme = colorschemes[1]
+while true do
+	local index = math.random(#colorschemes)
+	colorscheme = colorschemes[index]
+	if colorscheme.background ~= "dark" then
+		break
+	end
+end
+
+vim.g.randombones_colors_name = colorscheme.name
+
+vim.o.background = "light"
+vim.g.colors_name = colorscheme.name
+
+util.apply_colorscheme()
